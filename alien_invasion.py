57375e786c40d50f2a1b356a3a31e59ab6ddb1e6
Emzycodes <123588480+Emzycodes@users.noreply.github.com>
2023-11-14 00:55:39 +0100

    Updating the Game repo

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,23 @@
 import sys
 import pygame #imports the pygame lib
+from settings import Settings
 
 class AlienInvasion :
 
-    def __init(self):
+    def __init__(self):
         """ Initialize the game, and create a game resource"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings() 
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width,self.settings.screen_height
+        ))
         pygame.display.set_caption("Alien Invasion")
 
+        self.bg_color = (230,100,220)
+
     def run_game(self):
+        # Set the background color 
         """ Start the main loop for the game"""
         while True:
             # Watch for mouse and keyboard events.
@@ -18,9 +25,9 @@ class AlienInvasion :
                 if event.type == pygame.QUIT:
                     sys.exit()
             # # Make the most recently drawn screen visible.        
-            pygame.display.flip()\
+            pygame.display.flip()
             
 if __name__ == '__main__':
  # Make a game instance, and run the game.
- ai = AlienInvasion()
- ai.run_game()
+     ai = AlienInvasion()
+     ai.run_game()
